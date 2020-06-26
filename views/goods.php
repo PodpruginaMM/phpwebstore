ab4cf3f3637de50d2325b36040b580e770874a4b
podpruginamm <podpruginamm@gmail.com>
2020-06-26 23:37:44 +0300

    Minor problem with URLs for goods fixed

--- views/goods.php
+++ views/goods.php
@@ -3,15 +3,16 @@
 ?>
 
 
-
+<div id="app">
 <?php foreach($rows as $row) :?>
     <h2><?= $row['name'] ?></h2>
     <img style="width: 200px" src="/img/goods/<?= $row['img'] ?>" alt="img<?= $row['id'] ?>">
     <p><a href="?p=good&a=one&id=<?= $row['id'] ?>">Подробнее</a></p>
-    <button class="button" @click="send('<?= $row['id'] ?>')">В корзину</button><br>
+    <button class="button" @click="addGood(<?= $row['id'] ?>)">В корзину</button><br>
 <?php endforeach;?>
+</div>
 
-
+<!--
 <script>
     function send(id) {
         jQuery.ajax({
@@ -25,3 +26,27 @@
         });
     }
 </script>
+<button class="button" @click="send('<?= $row['id'] ?>')">В корзину</button><br>
+--> 
+
+<script>
+    new Vue({
+        el: '#app',
+        data: {
+            goodsId: '<?= getId()?>'
+        },
+        methods: {
+            addGood(id) {
+                let form = new FormData();
+                form.append('goodId', id);
+                axios.post(
+                    '?p=cart&a=axiosAdd',
+                    form
+                ).then(function (response) {
+                   console.log(response);
+                });
+            }
+        }
+    })
+</script>
+
