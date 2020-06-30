a10726d205dae24422068aee45a191b8c667f984
ap <a.informunity@gmail.com>
2020-06-30 14:58:42 +0300

    fixed auth crash and password input typr

---
+++ views/reg.php
@@ -0,0 +1,7 @@
+<p>Введите данные</p>
+<form method="post">
+    <input type="text" placeholder="fio" name="fio" >
+    <input type="text" placeholder="login" name="login">
+    <input type="password" placeholder="password" name="password">
+    <input type="submit">
+</form>
