a47997e3dd21b8fd738c7f64467e7567bb80460d
Mariya P <58138218+PodpruginaMM@users.noreply.github.com>
2020-06-30 15:01:38 +0300

    sql database updated

--- gbphp.sql
+++ gbphp.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Июн 25 2020 г., 23:12
--- Версия сервера: 8.0.12
--- Версия PHP: 7.2.10
+-- Хост: localhost
+-- Время создания: Июн 30 2020 г., 13:29
+-- Версия сервера: 10.4.13-MariaDB
+-- Версия PHP: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -41,11 +40,33 @@ CREATE TABLE `goods` (
 --
 
 INSERT INTO `goods` (`id`, `name`, `price`, `info`, `img`) VALUES
-(1, 'Товар1', '100', 'Информация о первом товаре', 'img-66df5.jpg'),
-(2, 'Товар2', '200', 'Информация о Втором товаре', 'img-66df5.jpg'),
-(3, 'Товар3', '300', 'Информация о Третьем товаре', 'img-66df5.jpg'),
-(4, 'Товар4', '400', 'Информация о Четвертом товаре', 'img-66df5.jpg'),
-(5, 'Товар5', '500', 'Информация о пятом товаре', 'img-66df5.jpg');
+(1, 'Японская картина Волны', '11000', 'Невероятное произведение искусства', 'waves.jpeg'),
+(2, 'Кошелек', '350', 'Ошеломительный элемент декора', 'purse.jpeg'),
+(3, 'Ковер-самолет', '19990', 'Этно ковер ручной работы', 'kazakh_carpet.jpg'),
+(4, 'Кеды Животное', '990', 'Уникальные Кеды с принтом', 'cool_sneakers.jpg'),
+(5, 'Пеньковый прямоугольник', '21000', 'Забытая картина брата Малевича', 'wooden_wall.jpg'),
+(6, 'Сумка Фродо', '6000', 'Уникальная сумка из натуральной кожи дракона', 'bag.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_name` text NOT NULL DEFAULT 'anonimus',
+  `cart` text NOT NULL,
+  `state` text NOT NULL DEFAULT 'paid'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='orders database';
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_name`, `cart`, `state`) VALUES
+(4, 'admin', '{\"5\":{\"name\":\"Пеньковый прямоугольник\",\"price\":\"21000\",\"count\":1,\"id\":\"5\"},\"6\":{\"name\":\"Сумка Фродо\",\"price\":\"6000\",\"count\":2,\"id\":\"6\"}}', 'Успешно доставлен'),
+(5, 'admin', '{\"6\":{\"name\":\"Сумка Фродо\",\"price\":\"6000\",\"count\":2,\"id\":\"6\"},\"5\":{\"name\":\"Пеньковый прямоугольник\",\"price\":\"21000\",\"count\":2,\"id\":\"5\"},\"4\":{\"name\":\"Кеды Животное\",\"price\":\"990\",\"count\":1,\"id\":\"4\"}}', 'Успешно доставлен');
 
 -- --------------------------------------------------------
 
@@ -58,7 +79,7 @@ CREATE TABLE `users` (
   `fio` varchar(50) NOT NULL COMMENT 'Фио',
   `login` varchar(50) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `is_admin` int(1) NOT NULL DEFAULT '0' COMMENT '0 - uesr, 1 - admin'
+  `is_admin` int(1) NOT NULL DEFAULT 0 COMMENT '0 - uesr, 1 - admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -75,7 +96,9 @@ INSERT INTO `users` (`id`, `fio`, `login`, `password`, `is_admin`) VALUES
 (19, '22', '22', '22', 0),
 (20, '22', '22', '22', 0),
 (21, '22', '22', '22', 0),
-(22, '1', '2', '3', 0);
+(22, '1', '2', '3', 0),
+(23, 'Jon Doe', 'test@gmail.com', 'mypass', 0),
+(24, 'Jon Doe', 'no@gmail.com', '$2y$10$dEmCQswAXyOah4uLvyTzOOMv5M7KDmmpFVWnI/9gKribHTz6K5KXq', 0);
 
 --
 -- Индексы сохранённых таблиц
@@ -85,6 +108,12 @@ INSERT INTO `users` (`id`, `fio`, `login`, `password`, `is_admin`) VALUES
 -- Индексы таблицы `goods`
 --
 ALTER TABLE `goods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -102,13 +131,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `goods`
 --
 ALTER TABLE `goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
